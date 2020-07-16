582a45a13a2b30f40d1edfbd67d862661f30510d
Mos Daniele <mdie2771@scs.ubbcluj.ro>
2020-07-16 18:37:20 +0300

    update

--- titanic_challenge/main.py
+++ titanic_challenge/main.py
@@ -1,11 +1,8 @@
 import numpy as np
 import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
-import os
-for dirname, _, filenames in os.walk('/data'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-data = pd.read_csv("train.csv")
-print("Train data head:\n", data.head(9))
+data_train = pd.read_csv("train.csv")
+print("Train data head:\n", data_train.head(9))
 
