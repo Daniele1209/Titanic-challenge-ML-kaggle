719c6145f60e217df801600b5016c442ed5f3f72
Mos Daniele <mdie2771@scs.ubbcluj.ro>
2020-07-16 11:35:17 +0300

    importing data

--- titanic_challenge/main.py
+++ titanic_challenge/main.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 
 import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
+for dirname, _, filenames in os.walk('/data'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
-data = pd.read_csv('/kaggle/input/titanic/train.csv')
+data = pd.read_csv("train.csv")
 print("Train data head:\n", data.head(9))
+
